897397de1e72f4b293ac2f8de23a2e042e6ddc75
XinghanYin <xy5@njit.edu>
2023-10-06 13:56:15 -0400

    Update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231006.000122';
+const cacheName = 'chirpy-1696614514';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
