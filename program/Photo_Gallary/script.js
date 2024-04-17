39e28af093ce4b5136d58f07e9e61b65d992ac01
XinghanYin <xy5@njit.edu>
2024-04-17 19:37:13 -0400

    1

--- program/Photo_Gallary/script.js
+++ program/Photo_Gallary/script.js
@@ -2,33 +2,49 @@ const galleryPhoto = document.getElementById('galleryPhoto');
 const fullscreenModal = document.getElementById('fullscreenModal');
 const fullscreenPhoto = document.getElementById('fullscreenPhoto');
 const closeBtn = document.getElementsByClassName('close')[0];
+const seriesInfo = document.getElementById('series');
 const cameraInfo = document.getElementById('camera');
 const lensInfo = document.getElementById('lens');
 const focalLengthInfo = document.getElementById('focalLength');
 const locationInfo = document.getElementById('location');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+const cdnUrlInput = document.getElementById('cdnUrl');
 
 let photos = [];
 let currentPhotoIndex = 0;
 
-fetch('photos.json')
-  .then(response => response.json())
-  .then(data => {
-    photos = data;
-    displayPhoto();
-  });
+cdnUrlInput.addEventListener('change', () => {
+  const cdnUrl = cdnUrlInput.value;
+  fetch('photos.json')
+    .then(response => response.json())
+    .then(data => {
+      photos = data.map(photo => ({
+        ...photo,
+        url: `${cdnUrl}${photo.url}`
+      }));
+      displayPhoto();
+    });
+});
 
 function displayPhoto() {
   const photo = photos[currentPhotoIndex];
   galleryPhoto.src = photo.url;
-  
+
+  seriesInfo.textContent = photo.series ? `Series: ${photo.series}` : '';
   cameraInfo.textContent = photo.camera ? `Camera: ${photo.camera}` : '';
   lensInfo.textContent = photo.lens ? `Lens: ${photo.lens}` : '';
   focalLengthInfo.textContent = photo.focalLength ? `Focal Length: ${photo.focalLength}` : '';
   locationInfo.textContent = photo.location ? `Location: ${photo.location}` : '';
 }
 
-function displayRandomPhoto() {
-  currentPhotoIndex = Math.floor(Math.random() * photos.length);
+function showPreviousPhoto() {
+  currentPhotoIndex = (currentPhotoIndex - 1 + photos.length) % photos.length;
+  displayPhoto();
+}
+
+function showNextPhoto() {
+  currentPhotoIndex = (currentPhotoIndex + 1) % photos.length;
   displayPhoto();
 }
 
@@ -41,8 +57,7 @@ function closeFullscreen() {
   fullscreenModal.style.display = "none";
 }
 
-galleryPhoto.addEventListener('click', () => {
-  displayRandomPhoto();
-  openFullscreen();
-});
+prevButton.addEventListener('click', showPreviousPhoto);
+nextButton.addEventListener('click', showNextPhoto);
+galleryPhoto.addEventListener('click', openFullscreen);
 closeBtn.addEventListener('click', closeFullscreen);
